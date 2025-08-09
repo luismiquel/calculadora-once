30aed966c2081331b8e4b39fe0ec4329ed830913
Luis Miguel <luismiguelgarciadelasmorenas@gmail.com>
2025-08-09 20:20:14 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,35 +1,25 @@
-// service-worker.js
-const CACHE_NAME = "calc-once-v11";
+const CACHE_NAME = "calc-once-v7";
 const ASSETS = [
   "./",
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./favicon.png"
 ];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
-  self.skipWaiting();
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
 });
 
-self.addEventListener("activate", (e) => {
+self.addEventListener("activate", e => {
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.map(k => (k !== CACHE_NAME ? caches.delete(k) : null)))
     )
   );
-  self.clients.claim();
 });
 
-self.addEventListener("fetch", (e) => {
-  const url = new URL(e.request.url);
-  const isAsset = ASSETS.some(a => url.pathname.endsWith(a.replace("./","/")));
-  if (isAsset) {
-    // Cache-first para assets
-    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-  } else {
-    // Network-first con fallback a index (modo SPA)
-    e.respondWith(
-      fetch(e.request).catch(() => caches.match("./index.html"))
-    );
-  }
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match("./index.html")))
+  );
 });
