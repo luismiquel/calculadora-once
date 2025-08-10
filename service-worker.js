6da383995211bc2cddac3415ba70613458fba871
Luis Miguel <luismiguelgarciadelasmorenas@gmail.com>
2025-08-10 10:53:14 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,12 @@
-const CACHE_NAME = "calc-once-v7";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./favicon.png"
-];
+// service-worker.js
+const CACHE_NAME = "calc-once-v12";
+const ASSETS = ["./","./index.html","./manifest.json"];
 
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
 });
 
-self.addEventListener("activate", e => {
+self.addEventListener("activate", (e) => {
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.map(k => (k !== CACHE_NAME ? caches.delete(k) : null)))
@@ -18,8 +14,8 @@ self.addEventListener("activate", e => {
   );
 });
 
-self.addEventListener("fetch", e => {
+self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match("./index.html")))
+    fetch(e.request).catch(() => caches.match(e.request).then(r => r || caches.match("./index.html")))
   );
 });
