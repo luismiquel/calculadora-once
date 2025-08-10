d8e574267d22151cceace0b1a5b1bc82332b8b6c
Luis Miguel <luismiguelgarciadelasmorenas@gmail.com>
2025-08-10 11:43:00 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,34 @@
 // service-worker.js
-const CACHE_NAME = "calc-once-v12";
-const ASSETS = ["./","./index.html","./manifest.json"];
+const CACHE = "calc-once-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
 
 self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
 
 self.addEventListener("activate", (e) => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.map(k => (k !== CACHE_NAME ? caches.delete(k) : null)))
+      Promise.all(keys.map(k => (k !== CACHE ? caches.delete(k) : null)))
     )
   );
 });
 
 self.addEventListener("fetch", (e) => {
+  // Estrategia: network-first con fallback a caché para index y assets
   e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request).then(r => r || caches.match("./index.html")))
+    fetch(e.request)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy)).catch(()=>{});
+        return res;
+      })
+      .catch(() => caches.match(e.request).then(r => r || caches.match("./index.html")))
   );
 });
